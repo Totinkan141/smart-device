399e654c6884a5f33122cc9e4e26f0b118e5b633
Dmitry Obryvkov <dmitry.obryvkov213@mail.ru>
2023-03-12 18:58:29 +0300

    Доделывает подвал сайта

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,7 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
+import {initAccardion} from './modules/accordion/accordion';
 
 // ---------------------------------
 
@@ -14,6 +15,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // Modules
   // ---------------------------------
 
+  initAccardion();
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
---
+++ source/js/modules/accordion/accordion.js
@@ -0,0 +1,24 @@
+
+
+function initAccardion() {
+  let accordion = document.querySelector('[data-accordion]');
+  let accordionItems = accordion.querySelectorAll('[data-accordion-item]');
+
+  accordionItems.forEach((element) => {
+    element.addEventListener('click', () => {
+      const parent = element.parentNode;
+
+      if (parent.classList.contains('is-active')) {
+        parent.classList.remove('is-active');
+      } else {
+        accordionItems.forEach((item) => {
+          item.parentNode.classList.remove('is-active');
+        });
+
+        parent.classList.add('is-active');
+      }
+    });
+  });
+}
+
+export {initAccardion};
