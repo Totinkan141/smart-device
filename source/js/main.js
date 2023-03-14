eb4c5ca0f4ac1781dda66f8e332043f5dc13c0c9
Dmitry Obryvkov <dmitry.obryvkov213@mail.ru>
2023-03-15 00:50:25 +0300

    Создаёт блок 'О компании', полностью адаптирует (резина), оживляет 'подробнее'

--- source/js/main.js
+++ source/js/main.js
@@ -2,7 +2,7 @@ import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
 import {initAccardion} from './modules/accordion/accordion';
-
+import {showMore} from './modules/about-more-info/more-info';
 // ---------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -16,6 +16,8 @@ window.addEventListener('DOMContentLoaded', () => {
   // ---------------------------------
 
   initAccardion();
+  showMore();
+
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
