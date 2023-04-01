9857726632523cf369af291ac9bf397f93dbf7dd
Dmitry Obryvkov <dmitry.obryvkov213@mail.ru>
2023-04-01 20:21:04 +0300

    Фиксит логику кнопки подробнее

--- source/js/modules/about-more-info/more-info.js
+++ source/js/modules/about-more-info/more-info.js
@@ -2,33 +2,24 @@ function showMore() {
   let aboutCompany = document.querySelector('[data-about-company]');
   if (aboutCompany) {
     let buttonShowMore = aboutCompany.querySelector('[data-about-button]');
+
     buttonShowMore.addEventListener('click', (evt) => {
       evt.preventDefault();
+      buttonShowMore.classList.toggle('is-active');
 
-      let hideItems = aboutCompany.querySelectorAll('[data-about-bonus-info]');
-      let count = 0;
+      const BONUS_ITEMS = aboutCompany.querySelectorAll('[data-about-bonus-info]');
+      let isActive = buttonShowMore.classList.contains('is-active');
 
-      hideItems.forEach((item) => {
-        if (item.style.display === 'none') {
-          count += 1;
-        }
-      });
-
-      if (count > 0) {
-        hideItems.forEach((item) => {
+      if (isActive) {
+        BONUS_ITEMS.forEach((item) => {
           item.style.display = 'inline-block';
         });
-        buttonShowMore.querySelector('[data-about-button-text="more"]').style.display = 'none';
-        buttonShowMore.querySelector('[data-about-button-text="close"]').style.display = 'inline-block';
-
       } else {
-        hideItems.forEach((item) => {
+        BONUS_ITEMS.forEach((item) => {
           if (!(item.classList.contains('about-company__text--long') && window.screen.width > 770)) {
             item.style.display = 'none';
           }
         });
-        buttonShowMore.querySelector('[data-about-button-text="more"]').style.display = 'inline-block';
-        buttonShowMore.querySelector('[data-about-button-text="close"]').style.display = 'none';
       }
     });
   }
